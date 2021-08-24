8349dc7cb6c21a27ab5e98815ed56aa84b06e816
Shantanu Yadav <shantanusk.sk@gmail.com>
2021-08-25 00:45:19 +0530

    added heroku file

--- quiz/settings.py
+++ quiz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oc^f2i)1#b_q8@trb@mdj+d%r@pm*156s*&ddj_nmm(u_h#8a*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
